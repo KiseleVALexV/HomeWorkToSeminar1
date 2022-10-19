93b417e94e57ebd60fd8a475a19257dd311b95c7
Alexey <manoil77@list.ru>
2022-10-19 12:04:24 +0300

    Add Zadacha008 Vse chetnye chisla ot 1 do N

---
+++ Zadacha008_VseChetnyeChislaOt1DoN/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+Console.WriteLine("Четные числа в заданном диапазоне : ");
+
+while (count <= number)
+{
+   if (count % 2 == 0) 
+   {
+    Console.Write($"{count} ");
+   }
+   count = count + 1;
+}
+
+Console.WriteLine();
+Console.ReadKey();
+
